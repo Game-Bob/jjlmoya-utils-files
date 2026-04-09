8bc9aa0c19d4bb689c42035931f4ee82e0460405
jjlmoya <juanjo1989@gmail.com>
2026-04-09 19:08:44 +0200

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ export type {
 
 export { ALL_TOOLS } from './tools';
 
-export { ClipboardToImageComponent, ClipboardToImageSEO, ClipboardToImageBibliography } from './tool/clipboardToImage';
-export { FormatStripperComponent, FormatStripperSEO, FormatStripperBibliography } from './tool/formatStripper';
-export { CharacterCounterComponent, CharacterCounterSEO, CharacterCounterBibliography } from './tool/characterCounter';
-export { HashVerifierComponent, HashVerifierSEO, HashVerifierBibliography } from './tool/hashVerifier';
-export { TextToMarkdownComponent, TextToMarkdownSEO, TextToMarkdownBibliography } from './tool/textToMarkdown';
-export { EmojiSpaceCleanerComponent, EmojiSpaceCleanerSEO, EmojiSpaceCleanerBibliography } from './tool/emojiSpaceCleaner';
-export { TextSplitterComponent, TextSplitterSEO, TextSplitterBibliography } from './tool/textSplitter';
-export { JsonToCsvComponent, JsonToCsvSEO, JsonToCsvBibliography } from './tool/jsonToCsv';
+export { ClipboardToImageComponent, ClipboardToImageSEO, ClipboardToImageBibliography, CLIPBOARD_TO_IMAGE_TOOL } from './tool/clipboardToImage';
+export { FormatStripperComponent, FormatStripperSEO, FormatStripperBibliography, FORMAT_STRIPPER_TOOL } from './tool/formatStripper';
+export { CharacterCounterComponent, CharacterCounterSEO, CharacterCounterBibliography, CHARACTER_COUNTER_TOOL } from './tool/characterCounter';
+export { HashVerifierComponent, HashVerifierSEO, HashVerifierBibliography, HASH_VERIFIER_TOOL } from './tool/hashVerifier';
+export { TextToMarkdownComponent, TextToMarkdownSEO, TextToMarkdownBibliography, TEXT_TO_MARKDOWN_TOOL } from './tool/textToMarkdown';
+export { EmojiSpaceCleanerComponent, EmojiSpaceCleanerSEO, EmojiSpaceCleanerBibliography, EMOJI_SPACE_CLEANER_TOOL } from './tool/emojiSpaceCleaner';
+export { TextSplitterComponent, TextSplitterSEO, TextSplitterBibliography, TEXT_SPLITTER_TOOL } from './tool/textSplitter';
+export { JsonToCsvComponent, JsonToCsvSEO, JsonToCsvBibliography, JSON_TO_CSV_TOOL } from './tool/jsonToCsv';
 
