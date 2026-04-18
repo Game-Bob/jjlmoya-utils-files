cef8084eff1606bf3b959cd87fa553b2bb743894
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:53 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { ClipboardToImageComponent, ClipboardToImageSEO, ClipboardToImageBibliography, CLIPBOARD_TO_IMAGE_TOOL } from './tool/clipboardToImage';
-export { FormatStripperComponent, FormatStripperSEO, FormatStripperBibliography, FORMAT_STRIPPER_TOOL } from './tool/formatStripper';
-export { CharacterCounterComponent, CharacterCounterSEO, CharacterCounterBibliography, CHARACTER_COUNTER_TOOL } from './tool/characterCounter';
-export { HashVerifierComponent, HashVerifierSEO, HashVerifierBibliography, HASH_VERIFIER_TOOL } from './tool/hashVerifier';
-export { TextToMarkdownComponent, TextToMarkdownSEO, TextToMarkdownBibliography, TEXT_TO_MARKDOWN_TOOL } from './tool/textToMarkdown';
-export { EmojiSpaceCleanerComponent, EmojiSpaceCleanerSEO, EmojiSpaceCleanerBibliography, EMOJI_SPACE_CLEANER_TOOL } from './tool/emojiSpaceCleaner';
-export { TextSplitterComponent, TextSplitterSEO, TextSplitterBibliography, TEXT_SPLITTER_TOOL } from './tool/textSplitter';
-export { JsonToCsvComponent, JsonToCsvSEO, JsonToCsvBibliography, JSON_TO_CSV_TOOL } from './tool/jsonToCsv';
+export { CLIPBOARD_TO_IMAGE_TOOL } from './tool/clipboardToImage';
+export { FORMAT_STRIPPER_TOOL } from './tool/formatStripper';
+export { CHARACTER_COUNTER_TOOL } from './tool/characterCounter';
+export { HASH_VERIFIER_TOOL } from './tool/hashVerifier';
+export { TEXT_TO_MARKDOWN_TOOL } from './tool/textToMarkdown';
+export { EMOJI_SPACE_CLEANER_TOOL } from './tool/emojiSpaceCleaner';
+export { TEXT_SPLITTER_TOOL } from './tool/textSplitter';
+export { JSON_TO_CSV_TOOL } from './tool/jsonToCsv';
 
